d98eae2901d0c5e6674096b9bce73d6ea538c424
WhiteHyun <cx_nf@naver.com>
2020-06-17 23:10:14 +0900

    버튼 입력값을 flag가 아닌 '숫자'로 리턴

--- btn.c
+++ btn.c
@@ -2,95 +2,94 @@
 
 int GetBtn(TLCD tlcdInfo, int xpos, int ypos)
 {
-    int inputBtnFlag = 0x00000;
+    int inputBtnFlag = 0;
 
-    // line flag Up
     if ((xpos >= 270 && xpos <= 320) && (ypos >= 175 && ypos <= 205))
     {
-        inputBtnFlag = 1; // 00 0000 0000 0000 0001 Pen
+        inputBtnFlag = TOUCH_PEN; // 00 0000 0000 0000 0001 Pen
     }
     else if ((xpos >= 270 && xpos <= 320) && (ypos >= 210 && ypos <= 240))
     {
-        inputBtnFlag = (1 << 1); // 00 0000 0000 0000 0010 Fill
+        inputBtnFlag = TOUCH_FILL; // 00 0000 0000 0000 0010 Fill
     }
     else if ((xpos >= 0 && xpos <= 50) && (ypos >= 0 && ypos <= 30))
     {
-        inputBtnFlag = (1 << 2); // 00 0000 0000 0000 0100 Line
+        inputBtnFlag = TOUCH_LINE; // 00 0000 0000 0000 0100 Line
     }
 
     else if ((xpos >= 0 && xpos <= 50) && (ypos >= 35 && ypos <= 65))
     {
-        inputBtnFlag = (1 << 3); // 00 0000 0000 0000 1000 Rectangle
+        inputBtnFlag = TOUCH_RECT; // 00 0000 0000 0000 1000 Rectangle
     }
 
     else if ((xpos >= 0 && xpos <= 50) && (ypos >= 70 && ypos <= 100))
     {
-        inputBtnFlag = (1 << 4); // 00 0000 0000 0001 0000 Oval
+        inputBtnFlag = TOUCH_OVAL; // 00 0000 0000 0001 0000 Oval
     }
 
     else if ((xpos >= 0 && xpos <= 50) && (ypos >= 105 && ypos <= 135))
     {
-        inputBtnFlag = (1 << 5); // 00 0000 0000 0010 0000 Free Draw
+        inputBtnFlag = TOUCH_FREEDRAW; // 00 0000 0000 0010 0000 Free Draw
     }
 
     else if ((xpos >= 0 && xpos <= 50) && (ypos >= 140 && ypos <= 170))
     {
-        inputBtnFlag = (1 << 6); // 00 0000 0000 0100 0000 Select
+        inputBtnFlag = TOUCH_SEL; // 00 0000 0000 0100 0000 Select
     }
 
     else if ((xpos >= 0 && xpos <= 50) && (ypos >= 175 && ypos <= 205))
     {
-        inputBtnFlag = (1 << 7); // 00 0000 0000 1000 0000 Erase
+        inputBtnFlag = TOUCH_ERASE; // 00 0000 0000 1000 0000 Erase
     }
 
     else if ((xpos >= 0 && xpos <= 50) && (ypos >= 210 && ypos <= 240))
     {
-        inputBtnFlag = (1 << 8); // 00 0000 0001 0000 0000 Clear
+        inputBtnFlag = TOUCH_CLEAR; // 00 0000 0001 0000 0000 Clear
     }
 
     else if ((xpos >= 272 && xpos <= 295) && (ypos >= 0 && ypos <= 42))
     {
-        inputBtnFlag = (1 << 9); // 00 0000 0010 0000 0000 WhiteColor
+        inputBtnFlag = TOUCH_WHITE; // 00 0000 0010 0000 0000 WhiteColor
     }
 
     else if ((xpos >= 297 && xpos <= 320) && (ypos >= 0 && ypos <= 42))
     {
-        inputBtnFlag = (1 << 10); // 00 0000 0100 0000 0000 OrangeColor
+        inputBtnFlag = TOUCH_ORANGE; // 00 0000 0100 0000 0000 OrangeColor
     }
 
     else if ((xpos >= 272 && xpos <= 295) && (ypos >= 44 && ypos <= 86))
     {
-        inputBtnFlag = (1 << 11); // 00 0000 1000 0000 0000 RedColor
+        inputBtnFlag = TOUCH_RED; // 00 0000 1000 0000 0000 RedColor
     }
 
     else if ((xpos >= 297 && xpos <= 320) && (ypos >= 44 && ypos <= 86))
     {
-        inputBtnFlag = (1 << 12); // 00 0001 0000 0000 0000 GreenColor
+        inputBtnFlag = TOUCH_GREEN; // 00 0001 0000 0000 0000 GreenColor
     }
 
     else if ((xpos >= 272 && xpos <= 295) && (ypos >= 88 && ypos <= 130))
     {
-        inputBtnFlag = (1 << 13); // 00 0010 0000 0000 0000 YellowColor
+        inputBtnFlag = TOUCH_YELLOW; // 00 0010 0000 0000 0000 YellowColor
     }
 
     else if ((xpos >= 297 && xpos <= 320) && (ypos >= 88 && ypos <= 130))
     {
-        inputBtnFlag = (1 << 14); // 00 0100 0000 0000 0000 NavyColor
+        inputBtnFlag = TOUCH_NAVY; // 00 0100 0000 0000 0000 NavyColor
     }
 
     else if ((xpos >= 272 && xpos <= 295) && (ypos >= 132 && ypos <= 174))
     {
-        inputBtnFlag = (1 << 15); // 00 1000 0000 0000 0000 BlueColor
+        inputBtnFlag = TOUCH_BLUE; // 00 1000 0000 0000 0000 BlueColor
     }
 
     else if ((xpos >= 297 && xpos <= 320) && (ypos >= 132 && ypos <= 174))
     {
-        inputBtnFlag = (1 << 16); // 00 0000 0100 0000 0000 BlackColor
+        inputBtnFlag = TOUCH_BLACK; // 00 0000 0100 0000 0000 BlackColor
     }
 
     else if ((xpos >= 60 && xpos <= 260) && (ypos >= 10 && ypos <= 230))
     {
-        inputBtnFlag = (1 << 17); // 10 0000 0000 0000 0000 DrawScreen
+        inputBtnFlag = TOUCH_CANVAS; // 10 0000 0000 0000 0000 DrawScreen
     }
 
     return inputBtnFlag;
@@ -101,21 +100,21 @@ void SensingTouch(TLCD tlcdInfo)
     int x, y, pressure;
     int xpos, ypos, ret;
 
-    read(tlcdInfo.fd, &ie, sizeof(struct input_event));
+    read(tlcdInfo.fd, &tlcdInfo.ie, sizeof(struct input_event));
 
-    if (ie.type == 3)
+    if (tlcdInfo.ie.type == 3)
     {
-        if (ie.code == 0)
+        if (tlcdInfo.ie.code == 0)
         {
-            x = ie.value;
+            x = tlcdInfo.ie.value;
         }
-        else if (ie.code == 1)
+        else if (tlcdInfo.ie.code == 1)
         {
-            y = ie.value;
+            y = tlcdInfo.ie.value;
         }
-        else if (ie.code == 24)
+        else if (tlcdInfo.ie.code == 24)
         {
-            pressure = ie.value;
+            pressure = tlcdInfo.ie.value;
         }
     }
 
