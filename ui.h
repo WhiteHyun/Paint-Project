a29705c33ef303b6765510ab60aade703211f187
WhiteHyun <cx_nf@naver.com>
2020-06-18 02:19:10 +0900

    색상에 대한 상수값 설정

--- ui.h
+++ ui.h
@@ -3,7 +3,18 @@
 #include "set.h"
 typedef unsigned char ubyte;
 
-unsigned short MakePixel(ubyte r, ubyte g, ubyte b);
+enum Color
+{
+    WHITE = 0xFFFF,
+    ORANGE = 0xFE00,
+    RED = 0xF800,
+    GREEN = 0x968A,
+    YELLOW = 0xFFE0,
+    NAVY = 0x10C,
+    BLUE = 0x398,
+    BLACK = 0x0,
+    MINT = 0x670
+};
 //기본 UI 출력
 void DrawUI(TLCD tlcdInfo);
 
