b2e9deef6cc29b932d18a0672b55416b5b1bce01
EDK <kde9604@naver.com>
2020-06-19 01:58:46 +0900

    cherry-pick btn.c

--- btn.c
+++ btn.c
@@ -1,6 +1,7 @@
 #include "btn.h"
 #include "draw.h"
 #include "list.h"
+#include "ui.h"
 
 int GetBtn(TLCD tlcdInfo, int xpos, int ypos)
 {
@@ -138,32 +139,54 @@ void SensingTouch(TLCD tlcdInfo)
     {
     case TOUCH_WHITE:
         printf("todo WhiteColor\n");
+        shape.outColor = WHITE; //Set OutBound Color WHITE (why!???)
         break;
     case TOUCH_ORANGE:
         printf("todo OrangeColor\n");
+        shape.outColor = ORANGE; //Set OutBound Color ORANGE
         break;
     case TOUCH_RED:
         printf("todo RedColor\n");
+        shape.outColor = RED; //Set OutBound Color RED
         break;
     case TOUCH_GREEN:
         printf("todo GreenColor\n");
+        shape.outColor = GREEN; //Set OutBound Color GREEN
         break;
     case TOUCH_YELLOW:
         printf("todo YellowColor\n");
+        shape.outColor = YELLOW; //Set OutBound Color YELLOW
         break;
     case TOUCH_NAVY:
         printf("todo NavyColor\n");
+        shape.outColor = NAVY; //Set OutBound Color NAVY
         break;
     case TOUCH_BLUE:
         printf("todo BlueColor\n");
+        shape.outColor = BLUE; //Set OutBound Color BLUE
         break;
     case TOUCH_BLACK:
         printf("todo BlackColor\n");
+        shape.outColor = BLACK; //Set OutBound Color BLACK
         break;
     case TOUCH_CANVAS:
         printf("touching screen %d %d\n", xpos, ypos);
         if (state >= 0 && state < 9)
         {
+            shape.outColor = 0;
+
+            // set Up Start x , y pos
+            if (isFirst == 1)
+            {
+                shape.start.x = xpos;
+                shape.start.y = ypos;
+
+                isFirst = -1;
+            }
+            // set Up End x , y pos
+            shape.end.x = xpos;
+            shape.end.y = ypos;
+
             g_drawTable[state](tlcdInfo, &shape);
             struct ListNode *node = CreateNode(shape);
             Append(node);
