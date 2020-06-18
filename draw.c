bbf6301b7e21cfbd24afb14b5705cd60788f8708
EDK <kde9604@naver.com>
2020-06-18 13:05:44 +0900

    add TODO

--- draw.c
+++ draw.c
@@ -1,5 +1,14 @@
 #include "draw.h"
 #include "ui.h"
+
+/*
+ * This is Base Code for Making Rectangle Made by T.H Kim
+ * Make start x , y -> end x , y Rectange 
+ * 필요한기능  -> 입력받은 점이 처음 찍힌 점일경우 start x, y에 적립
+ *             -> 아닐경우 end의 x,y좌표를 계속하여 갱신하여줍니다.
+ *             -> 갱신했을시 이전에 그려진 Box를 지워줍니다 (시작할떄 집어주면 될듯?)
+ *             -> ( 이 기능이 핵심적임 ) 
+ */
 void DrawRectangle(TLCD tlcdInfo, Shape shape)
 {
     int i, tmp, offset;
@@ -35,11 +44,28 @@ void DrawRectangle(TLCD tlcdInfo, Shape shape)
     }
 }
 
+
+/*
+ * This is Base Code for Making Rectangle Made by D.S Kim
+ * Make start x , y -> end x , y Rectange
+ * 필요한기능  -> 입력받은 점이 처음 찍힌 점일경우 start x, y에 적립
+ *             -> 아닐경우 end의 x,y좌표를 계속하여 갱신하여줍니다.
+ *             -> 갱신했을시 이전에 그려진 Line의 좌표를 특정하여 지워줍니다 (시작할떄 집어주면 될듯?)
+ *             -> ( 이 기능이 핵심적임 )
+ */
 void DrawLine(TLCD tlcdInfo, Shape shape)
 {
     /* TODO: Draw line */
 }
 
+/*
+ * This is Base Code for Making Rectangle Made by D.E Kime
+ * Make start x , y -> end x , y Rectange
+ * 필요한기능  -> 입력받은 점이 처음 찍힌 점일경우 start x, y에 적립
+ *             -> 아닐경우 end의 x,y좌표를 계속하여 갱신하여줍니다.
+ *             -> 갱신했을시 이전에 그려진 Oval을 지워줍니다 (시작할떄 집어주면 될듯?)
+ *             -> ( 이 기능이 핵심적임 )
+ */
 void DrawOval(TLCD tlcdInfo, Shape shape)
 {
     /* TODO: Draw Oval */
