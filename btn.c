31ecf46e4c3fe42aca7da547abff34d83f0fbe3e
Kim DaeSeong <kimds5344@naver.com>
2020-06-21 23:29:13 +0900

    가독성 수정

--- btn.c
+++ btn.c
@@ -36,10 +36,12 @@ int GetBtn(int xpos, int ypos)
     {
         inputBtnFlag = TOUCH_PEN;
     }
+    
     else if ((xpos >= 270 && xpos <= 320) && (ypos >= 210 && ypos <= 240))
     {
         inputBtnFlag = TOUCH_FILL;
     }
+    
     else if ((xpos >= 0 && xpos <= 50) && (ypos >= 0 && ypos <= 30))
     {
         inputBtnFlag = TOUCH_LINE;
@@ -129,6 +131,7 @@ void SensingTouch(TLCD *tlcdInfo)
     Shape shape;
     shape.position = NULL;
     InputTouch(tlcdInfo);
+    
     // 보정을 넣은 lcd상의 x , y의 포지션
     xpos = tlcdInfo->a * tlcdInfo->x + tlcdInfo->b * tlcdInfo->y + tlcdInfo->c;
     ypos = tlcdInfo->d * tlcdInfo->x + tlcdInfo->e * tlcdInfo->y + tlcdInfo->f;
@@ -138,48 +141,59 @@ void SensingTouch(TLCD *tlcdInfo)
     {
         ret = GetBtn(xpos, ypos);
     }
+    
     else
     {
         ret = -1;
     }
+    
     switch (ret)
     {
     case TOUCH_WHITE:
         printf("COLOR WHITE\n");
         selectedColor = WHITE;
         break;
+            
     case TOUCH_ORANGE:
         printf("COLOR ORANGE\n");
         selectedColor = ORANGE;
         break;
+            
     case TOUCH_RED:
         printf("COLOR RED\n");
         selectedColor = RED;
         break;
+            
     case TOUCH_GREEN:
         printf("COLOR GREEN\n");
         selectedColor = GREEN;
         break;
+            
     case TOUCH_YELLOW:
         printf("COLOR YELLOW\n");
         selectedColor = YELLOW;
         break;
+            
     case TOUCH_NAVY:
         printf("COLOR NAVY\n");
         selectedColor = NAVY;
         break;
+            
     case TOUCH_BLUE:
         printf("COLOR BLUE\n");
         selectedColor = BLUE;
         break;
+            
     case TOUCH_BLACK:
         printf("COLOR BLACK\n");
         selectedColor = BLACK;
         break;
+            
     case TOUCH_CLEAR: //클리어 모드
         g_drawTable[ret](tlcdInfo, &shape);
         mode = TOUCH_PEN; //화면 클리어 후 펜모드로 바꿈
         break;
+            
     case TOUCH_CANVAS:
         //펜모드이면서 그림유형도 선택되어있는 경우 정상적인 그림 그리기
         if (mode == TOUCH_PEN && state >= 0 && state < 4)
@@ -187,13 +201,16 @@ void SensingTouch(TLCD *tlcdInfo)
             //Set color
             shape.inColor = WHITE;
             shape.outColor = selectedColor;
+            
             // set Up Start x , y pos
             shape.start.x = xpos;
             shape.start.y = ypos;
+            
             g_drawTable[state](tlcdInfo, &shape);      //함수 불러짐
             struct ListNode *node = CreateNode(shape); //도형에 대한 노드를 만들어서
             Append(node);                              //리스트에 노드 저장
         }
+            
         //설정모드: 채우기 모드거나 선택 모드거나 지우기모드일 때
         else if (mode == TOUCH_FILL || mode == TOUCH_SEL || mode == TOUCH_ERASE)
         {
@@ -205,8 +222,11 @@ void SensingTouch(TLCD *tlcdInfo)
             shape.inColor = selectedColor; //FILL모드일 경우 inColor 속성을 가지고 파라미터로 전달함
             g_drawTable[mode](tlcdInfo, &shape);
         }
+            
         break;
+            
     default:
+            
         /*
          * 터치를 때면 ret이 -1이 되기 때문에
          * 항상 -1이 들어오는 오류 방지
@@ -217,12 +237,14 @@ void SensingTouch(TLCD *tlcdInfo)
             {
                 mode = ret;
             }
+            
             else //그리기 설정
             {
                 state = ret;
                 mode = TOUCH_PEN; //그리기 설정을 했을 경우 모드는 펜으로 자동설정
             }
         }
+            
         break;
     }
 }
