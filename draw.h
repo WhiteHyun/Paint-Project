e0896320ab881f679023a6fb0347e56f84b5fea8
Kim DaeSeong <kimds5344@naver.com>
2020-06-23 22:36:36 +0900

    객체넘버추가

--- draw.h
+++ draw.h
@@ -9,7 +9,74 @@ typedef struct _Point
     int y;
 } Point;
 
-/* 
+/*
+ * Shape infomation struct.
+ * shape -> 0 is Pen , 1 is Line , 2 Rectangle , 3 Oval , 4 FreeDraw
+ * Point start & end have each x , y pos
+ * inColor is inBound Color
+ * outColor is outBound Color
+ */
+typedef struct _Shape
+{
+    int gackcheNumber;
+    int type;
+    Point start;
+    Point end;
+    unsigned short inColor;  // inBound Color
+    unsigned short outColor; // outBound Color
+    int** position;          //FreeDraw array
+} Shape;
+
+typedef void (*drawList)(TLCD*, Shape*);
+
+void DrawLine(TLCD* tlcdInfo, Shape* shape);
+
+// make Rectangle Base Code
+void DrawRectangle(TLCD* tlcdInfo, Shape* shape);
+
+void DrawOval(TLCD* tlcdInfo, Shape* shape);
+
+void DrawFree(TLCD* tlcdInfo, Shape* shape);
+
+void DrawSelect(TLCD* tlcdInfo, Shape* shape);
+
+void DrawErase(TLCD* tlcdInfo, Shape* shape);
+
+void DrawClear(TLCD* tlcdInfo, Shape* shape);
+
+void DrawPen(TLCD* tlcdInfo, Shape* shape);
+
+void DrawFill(TLCD* tlcdInfo, Shape* shape);
+
+/*
+ * 도형과 관련된 함수들의 테이블입니다.
+ */
+static const drawList g_drawTable[] =
+{
+    DrawLine,
+    DrawRectangle,
+    DrawOval,
+    DrawFree,
+    DrawSelect,
+    DrawErase,
+    DrawClear,
+    DrawPen,
+    DrawFill
+};
+
+#endif
+#ifndef DRAW_H
+#define DRAW_H
+
+#include "set.h"
+
+typedef struct _Point
+{
+    int x;
+    int y;
+} Point;
+
+/*
  * Shape infomation struct.
  * shape -> 0 is Pen , 1 is Line , 2 Rectangle , 3 Oval , 4 FreeDraw
  * Point start & end have each x , y pos
@@ -23,34 +90,34 @@ typedef struct _Shape
     Point end;
     unsigned short inColor;  // inBound Color
     unsigned short outColor; // outBound Color
-    int **position;          //FreeDraw array
+    int** position;          //FreeDraw array
 } Shape;
 
-typedef void (*drawList)(TLCD *, Shape *);
+typedef void (*drawList)(TLCD*, Shape*);
 
-void DrawLine(TLCD *tlcdInfo, Shape *shape);
+void DrawLine(TLCD* tlcdInfo, Shape* shape);
 
 // make Rectangle Base Code
-void DrawRectangle(TLCD *tlcdInfo, Shape *shape);
+void DrawRectangle(TLCD* tlcdInfo, Shape* shape);
 
-void DrawOval(TLCD *tlcdInfo, Shape *shape);
+void DrawOval(TLCD* tlcdInfo, Shape* shape);
 
-void DrawFree(TLCD *tlcdInfo, Shape *shape);
+void DrawFree(TLCD* tlcdInfo, Shape* shape);
 
-void DrawSelect(TLCD *tlcdInfo, Shape *shape);
+void DrawSelect(TLCD* tlcdInfo, Shape* shape);
 
-void DrawErase(TLCD *tlcdInfo, Shape *shape);
+void DrawErase(TLCD* tlcdInfo, Shape* shape);
 
-void DrawClear(TLCD *tlcdInfo, Shape *shape);
+void DrawClear(TLCD* tlcdInfo, Shape* shape);
 
-void DrawPen(TLCD *tlcdInfo, Shape *shape);
+void DrawPen(TLCD* tlcdInfo, Shape* shape);
 
-void DrawFill(TLCD *tlcdInfo, Shape *shape);
+void DrawFill(TLCD* tlcdInfo, Shape* shape);
 
 /*
  * 도형과 관련된 함수들의 테이블입니다.
  */
-static const drawList g_drawTable[] = 
+static const drawList g_drawTable[] =
 {
     DrawLine,
     DrawRectangle,
